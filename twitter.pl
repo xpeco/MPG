49008765a819cf856d1dfbb24d54fc389fb6e69f
Martin Vukovic <mvukovic@microbotica.es>
2011-06-16 15:39:13 +0200

    Uncommented

--- twitter.pl
+++ twitter.pl
@@ -21,7 +21,7 @@ while(1){
         {
           print "New direct message\n";
           $object->{lastid}=$status->{id}; # store the last direct_message Id to not repeat
-#         $object->_updateLastId();
+          $object->_updateLastId();
 
           print "Tweet nº: $status->{id}\n Created at $status->{created_at}\n By: <$status->{sender}->{screen_name}>\n Content: $status->{text}\n";
           my $data;
@@ -35,16 +35,16 @@ while(1){
           if ($data->{message}=~/$object->{action}->{trigger}/i)
 			 {
              my $texto=$object->_action($data->{from});
-#				 my $result= eval {$object->{twitter}->new_direct_message($data->{from},$texto)};
+ 				 my $result= eval {$object->{twitter}->new_direct_message($data->{from},$texto)};
              if ($@=~/Parece que ya has dicho eso/)
 				 {
 						$texto.=' Y que no se repita';
-#						$result= eval {$object->{twitter}->new_direct_message($data->{from},$texto)};
+						$result= eval {$object->{twitter}->new_direct_message($data->{from},$texto)};
 	               warn "$@\n" if $@;
 				 }
 				 else {warn "$@\n" if $@;}
 			 }
-			 #my $destroy=eval {$object->{twitter}->destroy_direct_message($status->{id});} warn "@\n" if $@;
+			 my $destroy=eval {$object->{twitter}->destroy_direct_message($status->{id});} warn "@\n" if $@;
 
         }
 		  else {print "already read\n";}
