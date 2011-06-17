40a12cd6b0b28c73efff7505ae369f15efaa0ee8
Martin Vukovic <mvukovic@microbotica.es>
2011-06-17 13:40:16 +0200

    Install script for Ubuntu

---
+++ install_script_ubuntu.sh
@@ -0,0 +1,47 @@
+#!/bin/bash
+if [[ $EUID -ne 0 ]]; then
+   echo "This script must be run as root" 1>&2
+   exit 1
+fi
+
+PERLVER=`perl -v | awk '/This/ {print $4}' | sed -e 's/v//'`
+
+echo ""
+echo "Creating folders..."
+echo ""
+
+echo "/etc/MPG"
+	mkdir -p /etc/MPG
+
+echo "/usr/local/lib/perl5/MPG"
+	mkdir -p /usr/lib/perl/$PERLVER/MPG
+
+echo "/usr/local/bin"
+	mkdir -p /usr/local/bin
+
+echo ""
+echo "Copying files..."
+echo ""
+
+echo "config.xml -> /etc/MPG"
+	cp -f config.xml /etc/MPG/
+
+echo "MPGMail.pm -> /usr/lib/perl/$PERLVER/MPG"
+	cp -f MPGMail.pm /usr/lib/perl/$PERLVER/MPG/
+	
+echo "MPGTwitter.pm -> /usr/lib/perl/$PERLVER/MPG"
+	cp -f MPGTwitter.pm /usr/lib/perl/$PERLVER/MPG	
+
+
+echo "writemail.pl -> /usr/local/bin"
+	cp -f writemail.pl /usr/local/bin/
+
+echo "MPGDaemon.pl -> /usr/local/bin"
+	cp -f MPGDaemon.pl /usr/local/bin/
+	
+echo "twitter.pl -> /usr/local/bin"
+	cp -f twitter.pl /usr/local/bin/	
+
+echo ""
+echo "Done."
+echo ""
