a8ceac24ad1ef40afb1ab694c66137379b4da3b1
Juan José 'Peco' San Martín <jsanmartin@nubeprint.com>
2011-07-16 12:58:35 +0200

    Try to prevent Mysql failures

--- MPGTwitter.pm
+++ MPGTwitter.pm
@@ -33,80 +33,103 @@ sub _initdb{
        $self->{xml}->{password});
        
        if (not $self->{db}){
-         print STDERR "Connection to DB failed :-(\n";
-         exit(0);
+         print STDERR "Connection to DB failed :-( ...\n";
+	 my $error=1;
+         while($error){
+	  print "Do not worry, trying to reconnect to DB on _initdb\n";
+          sleep 5;
+          $self->{db}=DBI->connect("DBI:mysql:$self->{xml}->{database}:$self->{xml}->{host}",
+          $self->{xml}->{user},
+          $self->{xml}->{password});
+          if (defined $self->{db}){$error=0;print "Fixed\n";}
+         }
        }
 }
 
 sub _initdbaction{
-		 my $self=shift;
-       my $do=$self->{db}->prepare("select * from TwitterAction where Status='Enabled'");
+      my $self=shift;
+      my $do=$self->{db}->prepare("select * from TwitterAction where Status='Enabled'");
        $do->execute;
        my $records=$do->fetchall_arrayref({});
-
-		 $self->{dbaction}=DBI->connect("DBI:mysql:$records->[0]->{SQL_Table}:$records->[0]->{SQL_Host}",$records->[0]->{SQL_User},$records->[0]->{SQL_Pass});
-		 $self->{action}->{query}=$records->[0]->{SQL_Query};
-		 $self->{action}->{answer}=$records->[0]->{SQL_Answer};
-		 $self->{action}->{trigger}=$records->[0]->{Trigger};
+       $self->{dbaction}=DBI->connect("DBI:mysql:$records->[0]->{SQL_Table}:$records->[0]->{SQL_Host}",$records->[0]->{SQL_User},$records->[0]->{SQL_Pass});
+       $self->{action}->{query}=$records->[0]->{SQL_Query};
+       $self->{action}->{answer}=$records->[0]->{SQL_Answer};
+       $self->{action}->{trigger}=$records->[0]->{Trigger};
 
        if (not $self->{dbaction}){
          print STDERR "Connection to DB TwitterAction failed :-(\n";
-         exit(0);
+	 my $error=1;
+         while($error){
+	  print "Do not worry, trying to reconnect to DB\n";
+          sleep 5;
+          $self->{dbaction}=DBI->connect("DBI:mysql:$records->[0]->{SQL_Table}:$records->[0]->{SQL_Host}",$records->[0]->{SQL_User},$records->[0]->{SQL_Pass});
+          $self->{action}->{query}=$records->[0]->{SQL_Query};
+          $self->{action}->{answer}=$records->[0]->{SQL_Answer};
+          $self->{action}->{trigger}=$records->[0]->{Trigger};
+          if ($self->{dbaction}){$error=0;}
+         }
        }
 }
 
-
-
 sub _inittwitter{
        my $self=shift;
+       if (not $self->{db}) { print "Flag 1"; $self->_initdb; }
+
        my $do=$self->{db}->prepare("select * from TwitterConfig where Status='Enabled'");
        $do->execute;
+       if (not $self->{db}){print "Flag 1bis..RELEVANT..\n";}
+
        my $records=$do->fetchall_arrayref({});
        $self->{account}=$records->[0]->{ACCOUNT}; 
        $self->{password}=$records->[0]->{PASSWORD};
        $self->{frequency}=$records->[0]->{Frequency}; 
        $self->{lastid}=$records->[0]->{LastID}; 
        $self->{idreg}=$records->[0]->{Id}; 
-
-		 $self->{twitter} = Net::Twitter::Lite->new(
-    			consumer_key        => $records->[0]->{consumer_key},
-			   consumer_secret     => $records->[0]->{consumer_secret},
-				access_token        => $records->[0]->{access_token},
-				access_token_secret => $records->[0]->{access_token_secret}
+       print "Connecting to Twitter Server\n";
+       $self->{twitter} = Net::Twitter::Lite->new(
+                       consumer_key => $records->[0]->{consumer_key},
+                       consumer_secret => $records->[0]->{consumer_secret},
+                       access_token => $records->[0]->{access_token},
+                       access_token_secret => $records->[0]->{access_token_secret}
 		 );
+	if($@){print "Error: $@\n";}
 }
 
 sub updateLastId{
-		my $self=shift;
+      my $self=shift;
+      if (not $self->{db}) { print "Flag 2"; $self->_initdb; }
+
       my $do=$self->{db}->prepare("update TwitterConfig set LastID=\'$self->{lastid}\' where Id=\'$self->{idreg}\'");
       $do->execute;
+
 }
 
 sub _insertmessage{
-		my $self=shift;
+      my $self=shift;
       my $data=shift;
+      if (not $self->{db}) { print "Flag 3"; $self->_initdb; }
+
       my $do=$self->{db}->prepare("insert into TwitterMessages (`From`,`Message`) VALUES (\'$data->{from}\',\'$data->{message}\');");
       $do->execute;
-		return 1;
+      return 1;
 }
 
 sub _action{
        my $self=shift;
        my $user=shift;
-		 my $var='desconocido';
+       my $var='desconocido';
        my $query=$self->{action}->{query};
        $query=~s/_USER_/\'$user\'/;
+       if (not $self->{db}) { print "Flag 4"; $self->_initdb; }
        my $do=$self->{dbaction}->prepare($query);
        $do->execute;
        my $records=$do->fetchall_arrayref({});
-		 if(@$records!=0){
-       	$var=$records->[0]->{VAR};
-		 }
+       if(@$records!=0){
+         $var=$records->[0]->{VAR};
+       }
        my $string=$self->{action}->{answer};
        $string=~s/_VAR_/$var/g;
-
-		 return $string;
+       return $string;
 }
 
 1;
-
--- twitter.pl
+++ twitter.pl
@@ -11,44 +11,52 @@ while(1){
    eval {
 #      my $statuses = $nt->direct_messages({ since_id => 1, count => 3 });
       $statuses = $object->{twitter}->direct_messages(); # last 20
-   }; warn "$@\n" if $@;
+   };
 
-   if (@$statuses!=0)
+   if($@)
    {
-	   foreach my $status (reverse @$statuses)
-      {
-        if ($status->{id} > $object->{lastid})
+     print "Error from Twitter Server: $@\n";
+     print "Don't worry, we try to reconnect it\n";
+   }
+   else{
+        print "Read if something...\n";
+        if ($statuses!=0)
         {
-          print "New direct message\n";
-          $object->{lastid}=$status->{id}; # store the last direct_message Id to avoid repeating
-          $object->updateLastId();
+           foreach my $status (reverse @$statuses)
+           {
+              if ($status->{id} > $object->{lastid})
+              {
+                 print "New direct message\n";
+                 $object->{lastid}=$status->{id}; # store the last direct_message Id to avoid repeating
+                 $object->updateLastId();
 
-          print "Tweet nº: $status->{id}\n Created at $status->{created_at}\n By: <$status->{sender}->{screen_name}>\n Content: $status->{text}\n";
-          my $data;
-          $data->{content}=Dumper($status);
-          $data->{message}=$status->{text};
-          #$data->{from}=$status->{user}{screen_name};
-          $data->{from}=$status->{sender}->{screen_name};
+                 print "Tweet nº: $status->{id}\n Created at $status->{created_at}\n By: <$status->{sender}->{screen_name}>\n Content: $status->{text}\n";
+                 my $data;
+                 $data->{content}=Dumper($status);
+                 $data->{message}=$status->{text};
+                 #$data->{from}=$status->{user}{screen_name};
+                 $data->{from}=$status->{sender}->{screen_name};
 
-          $object->_insertmessage($data);
+                 $object->_insertmessage($data);
 
-          if ($data->{message}=~/$object->{action}->{trigger}/i)
-			 {
-             my $texto=$object->_action($data->{from});
- 				 my $result= eval {$object->{twitter}->new_direct_message($data->{from},$texto)};
-             if ($@=~/Parece que ya has dicho eso/)
-				 {
-						$texto.=' (Cuidado, ya has pedido tu saldo hace un momento)';
-						$result= eval {$object->{twitter}->new_direct_message($data->{from},$texto)};
-	               warn "$@\n" if $@;
-				 }
-				 else {warn "$@\n" if $@;}
-			 }
-			 my $destroy=eval {$object->{twitter}->destroy_direct_message($status->{id})}; warn "@\n" if $@;
+                 if ($data->{message}=~/$object->{action}->{trigger}/i)
+                 {
+                    my $texto=$object->_action($data->{from});
+                    my $result= eval {$object->{twitter}->new_direct_message($data->{from},$texto)};
+                    if ($@=~/Parece que ya has dicho eso/)
+                    {
+                       $texto.=' (Cuidado, ya has pedido tu saldo hace un momento)';
+                       $result= eval {$object->{twitter}->new_direct_message($data->{from},$texto)};
+                       warn "$@\n" if $@;
+                    }
+                    else {warn "$@\n" if $@;}
+                 }
+                 my $destroy=eval {$object->{twitter}->destroy_direct_message($status->{id})}; warn "@\n" if $@;
 
-        }
-		  else {print "already read\n";}
-      }
+              }
+              else {print "already read\n";}
+           }
+        } # if message
    }
 print "looping\n";
 sleep $object->{frequency};
