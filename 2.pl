eb473888a7d7baaef879beff0f01da404a615b78
Juan José 'Peco' San Martín <jsanmartin@nubeprint.com>
2012-09-25 16:44:45 +0200

    Minor

--- 2.pl
+++
@@ -1,5 +0,0 @@
-#!/usr/bin/perl
- use Mail::Sender;
- my $sender = new Mail::Sender {smtp => 'smtp.rioservices.fr'};
- die "Error: $Mail::Sender::Error\n" unless ref $sender;
- print join(', ', $sender->QueryAuthProtocols()),"\n";
